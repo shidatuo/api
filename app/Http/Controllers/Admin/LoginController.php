6d7b1c3e48aeda80b6b34e5bb9e7bbdbcde1e5f9
shidatuo <1003230257@qq.com>
2018-12-07 22:53:21 +0800

    添加admin.login 中间件

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -1,13 +1,30 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
-    //
+    /**
+     * @param OauthUser $oauthUserModel
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @author shidatuo
+     * @description 后台登陆页面
+     */
+    public function index()
+    {
+//        return 4444;
+        // 获取是否有第三方用户被设置为管理员
+//        $count = $oauthUserModel->where('is_admin', 1)->count();
+//        // 如果有第三方账号管理员；则通过第三方账号登录
+//        if ($count) {
+//            die('请通过第三方账号登录');
+//        } else {
+//            return view('admin.login.index');
+//        }
+    }
 
-    
+
 }
