f9f66d75d0a7061d5c99c0dc3a1476b844899835
shidatuo <1003230257@qq.com>
2018-12-23 19:40:00 +0800

    后台退出登陆

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -13,9 +13,7 @@ class LoginController extends Controller
      * @author shidatuo
      * @description 后台登陆页面
      */
-    public function index()
-    {
-//        return 4444;
+    public function index(){
         // 获取是否有第三方用户被设置为管理员
 //        $count = $oauthUserModel->where('is_admin', 1)->count();
 //        // 如果有第三方账号管理员；则通过第三方账号登录
@@ -25,6 +23,19 @@ class LoginController extends Controller
             return view('admin.login.index');
 //        }
     }
+
+    /**
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @author shidatuo
+     * @description 退出登陆
+     */
+    public function logout(){
+        Auth::logout();
+        //>删除session
+        session()->forget('user');
+        return redirect('admin/login/index');
+    }
+
 
 
 }
