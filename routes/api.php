ae65cce35d73db9982f019e1441a810303e9ce81
shidatuo <1003230257@qq.com>
2019-01-18 23:54:13 +0800

    上传代码

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('articles', function() {
+    
     // If the Content-Type and Accept headers are set to 'application/json',
     // this will return a JSON structure. This will be cleaned up later.
 //    return ;
