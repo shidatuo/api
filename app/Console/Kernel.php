563180964915f698acec4218b17047467883e24d
shidatuo <1003230257@qq.com>
2019-02-02 23:47:07 +0800

    上传代码 , 修复定时任务报错

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,15 +33,17 @@ class Kernel extends ConsoleKernel
                 ->where(["state"=>1])
                 ->get();
             foreach ($result as $values){
-                save("jy_sale_goods","id={$values->id}&state=3");
+                DB::table("jy_sale_goods")->where(['id'=>$values->id])->update(['state'=>3]);
+//                save("jy_sale_goods","id={$values->id}&state=3");
                 $order_list = DB::table("jy_order")->where(["goods_id"=>$values->id])->get();
                 foreach ($order_list as $value){
-                    $order_info = get("jy_order","id={$value->id}&single=true&fields=id,transaction_id");
-                    $log = "\n接收到的退款订单号为 : [{$order_info['id']}]";
-                    $refundOrder['refundNo'] = $order_info['id'];//我们的订单id
-                    $refundOrder['transactionId'] = $order_info['transaction_id'];
-                    $refundOrder['totalFee'] = (int)((string)($order_info['amount'] * 100));
-                    $refundOrder['refundFee'] = (int)((string)($order_info['amount'] * 100)); //微信是以分为单位
+//                    $order_info = get("jy_order","id={$value->id}&single=true&fields=id,transaction_id");
+                    $order_info = DB::table("jy_order")->select("id","transaction_id")->where(['id'=>$value->id])->first();
+                    $log = "\n接收到的退款订单号为 : [{$order_info->id}]";
+                    $refundOrder['refundNo'] = $order_info->id;//我们的订单id
+                    $refundOrder['transactionId'] = $order_info->transaction_id;
+                    $refundOrder['totalFee'] = (int)((string)($order_info->amount * 100));
+                    $refundOrder['refundFee'] = (int)((string)($order_info->amount * 100)); //微信是以分为单位
                     $log .= "\n接收到的退款参数为 : ".json_encode($refundOrder) . PHP_EOL;
                     $config['AppId'] = "wx6e75e53e4a50bf41";
                     $config['wx_v3_key'] = "mykjsde34sdfmzf98342559kdshzx8as";
@@ -57,13 +59,13 @@ class Kernel extends ConsoleKernel
                     $log .= "\n调用退款接口返回值为 : ".json_encode($return_refundOrder) . PHP_EOL;
                     //返回这个代表请求成功
                     if(isset($return_refundOrder['result_code']) && $return_refundOrder['result_code'] == 'SUCCESS'){
-                        $log .= "\n订单号[{$order_info['id']}] ------ 退款成功 ------" . PHP_EOL;
+                        $log .= "\n订单号[{$order_info->id}] ------ 退款成功 ------" . PHP_EOL;
                         log_ex('getOrderRefund.log',"$log\n=========== 进入到订单退款方法  END =============\n");
                         save("jy_order","id={$value->id}&is_refund=1");
                     }else{
                         $description = isset($return_refundOrder['err_code_des']) ? $return_refundOrder['err_code_des'] : '';
-                        $log .= "\n订单号[{$order_info['id']}] ------ 退款失败 {$order_info['amount']} (单位:元) " . PHP_EOL;
-                        $log .= "\n订单号[{$order_info['id']}] ------ 失败原因 : {$description} " . PHP_EOL;
+                        $log .= "\n订单号[{$order_info->id}] ------ 退款失败 {$order_info->amount} (单位:元) " . PHP_EOL;
+                        $log .= "\n订单号[{$order_info->id}] ------ 失败原因 : {$description} " . PHP_EOL;
                         log_ex('getOrderRefund.log',"$log\n=========== 进入到订单退款方法  END =============\n");
                     }
                 }
