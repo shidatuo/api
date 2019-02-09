df84114a4930b393b92e5a4b64e1223df5bee9de
shidatuo <1003230257@qq.com>
2019-02-10 01:05:38 +0800

    上传代码

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -987,16 +987,16 @@ class ApiController extends Controller{
          $notify_url = "https://shidatuos.cn/api/wxnotifyurl";
          $order_info = get("jy_order","id={$data['id']}&single=true&fields=amount,goods_id,num");
 
-         if(isset($order_info['goods_id']) && isINT($order_info['goods_id'])){
-             $sale_goods_info = get("jy_sale_goods","id={$order_info['goods_id']}&single=true&fields=actual_stock");
-             $sale_goods_info['actual_stock'];
-             if(!$sale_goods_info)
-                 jsonReturn(201,"商品不存在");
-             if(isset($sale_goods_info['actual_stock']) && $sale_goods_info['actual_stock'] <= 0)
-                 jsonReturn(201,"库存不足 , 已售完");
-             if(isset($sale_goods_info['actual_stock']) && $order_info['num'] > $sale_goods_info['actual_stock'])
-                 jsonReturn(202,"库存不足");
-         }
+//         if(isset($order_info['goods_id']) && isINT($order_info['goods_id'])){
+//             $sale_goods_info = get("jy_sale_goods","id={$order_info['goods_id']}&single=true&fields=actual_stock");
+//             $sale_goods_info['actual_stock'];
+//             if(!$sale_goods_info)
+//                 jsonReturn(201,"商品不存在");
+//             if(isset($sale_goods_info['actual_stock']) && $sale_goods_info['actual_stock'] <= 0)
+//                 jsonReturn(201,"库存不足 , 已售完");
+//             if(isset($sale_goods_info['actual_stock']) && $order_info['num'] > $sale_goods_info['actual_stock'])
+//                 jsonReturn(202,"库存不足");
+//         }
 
          $total_fee = isset($order_info['amount']) && $order_info['amount'] > 0 ? $order_info['amount'] : 0;
          $total_fee = $total_fee * 100;
