d4e9858f0d669daa1a6e1bbec196125cdc6a4465
史大坨 <1003230257@qq.com>
2019-03-13 21:33:25 +0800

    上传代码 , 每日打卡

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Api\ApiController;
 include_once __DIR__ . DIRECTORY_SEPARATOR . '../Libraries/Functions/helpers.php';
 include_once __DIR__ . DS . '../..' . DS . 'common' . DS . 'index.php';
 
+
 class Kernel extends ConsoleKernel
 {
     /**
